b963997e2ee624c669fe4212df0cf56bc2bbbde3
Lukas Höpfner <lukas.hoepfner@yahoo.de>
2018-09-27 16:33:11 +0200

    oversaw a conflict

--- server/index.js
+++ server/index.js
@@ -5,11 +5,7 @@ const port = 3000;
 app.get('/', (request, response) => {
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	setTimeout(function() {
-<<<<<<< HEAD
 		response.send('<p>some html</p>');
-=======
-		response.send('<p>html</p>');
->>>>>>> c3d1fa38b0900927bcac09a8051c695dedd1ef1d
 	}, 50);
 });
 
